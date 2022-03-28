052c4feb0f65aa674b6f2ee2ceffeb68016fe7c8
Nam Hyuck Kim <skagur0329@gmail.com>
2022-03-28 21:45:00 +0900

    Added src/lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 use std::env;
+use std::process;
+use minigrep::Config;
 
 fn main(){
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
-
-    println!("Arguments: {:?}", args);
-    println!("Searching for: {}", query);
-    println!("In File: {}", filename);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem passing arguments: {}", err);
+        process::exit(1);
+    });
+    if let Err(e) = minigrep::run(config){
+        println!("Applicaton Error: {}", e);
+        process::exit(1);
+    };
 }
+
+
