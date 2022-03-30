832267c754cb75cb2e28df2fb21db79d276eed28
Nam Hyuck Kim <skagur0329@gmail.com>
2022-03-30 13:00:44 +0900

    Added case (in)sensitive func

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,11 @@ fn main(){
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem passing arguments: {}", err);
+        eprintln!("Problem passing arguments: {}", err);
         process::exit(1);
     });
     if let Err(e) = minigrep::run(config){
-        println!("Applicaton Error: {}", e);
+        eprintln!("Applicaton Error: {}", e);
         process::exit(1);
     };
 }
